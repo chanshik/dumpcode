61dff5fb64586b188c7902efdfc2957a7bc22ec2
Chan Shik Lim <chanshik@gmail.com>
2015-02-22 00:14:58 +0900

    Initial commit.

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"./dumpcode"
+	"io/ioutil"
+)
+
+func main() {
+	if data, e := ioutil.ReadFile("./dumpcode/dumpcode.go"); e == nil {
+		dumpcode.DumpCode(([]byte)(data), len(data))
+	}
+}
